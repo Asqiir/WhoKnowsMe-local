2027dd19d20d0f6792c894fa724dcac9c88dd027
asqiir <asqiir@blaupost.de>
2018-06-19 20:34:17 +0200

    initial code

---
+++ backend.js
@@ -0,0 +1,8 @@
+function backend() {
+	function checkAccount(username) {
+		//1) get all portals? (prototypes?)
+		//2) test for any of these, whether there is an account
+		//3) put all accounts in a json
+		//4) return that json
+	}
+}
---
+++ frontend.js
@@ -0,0 +1,55 @@
+jQuery(function() {
+
+function run() { //run, when input event happens
+	//1) read input
+	const input = frontend.getInput();
+
+	//2) do work -> get answer as json? [extra file]
+	const json = backend.checkAccount(input);
+
+	//3) convert answer to list
+	const list = frontend.createList(json);
+
+	//3) print output
+	jQuery('#output').html(list);
+}
+
+
+
+function frontend() {
+	function getInput() {
+		return escape(jQuery('#username').val());
+	}
+
+	//====HELPER=FUNCTIONS========
+
+	function createList(json) {
+		const list = document.createElement('ul');
+
+		for(var acc in json) {
+			var listPoint = document.createElement('li');
+			listPoint.innerHTML = 'There is an <a href="' + json[acc].url + '">account</a> at ' + json[acc].name;
+			list.append(listPoint);
+		}
+
+		return list;
+	}
+
+
+	//====INPUT=EVENTS==============
+
+	//this function will be run when enter is pressed in the #username input
+	jQuery('#username').keypress(function (e) {
+		if (e.which == 13) {
+   			run();
+   		}
+	});
+
+	//this function will run when button #submit is clicked
+	jQuery('#submit').click(function() {
+		run();	
+	});
+}
+
+
+});
